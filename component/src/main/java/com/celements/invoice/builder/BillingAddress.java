1eb75cf9d3614bbb04eb8757c27ae725fa021e65
ebeutler <edoardo.beutler@synventis.com>
2014-06-22 17:35:29 +0200

    implement BillingAddress

---
+++ component/src/main/java/com/celements/invoice/builder/BillingAddress.java
@@ -0,0 +1,104 @@
+package com.celements.invoice.builder;
+
+public class BillingAddress implements IBillingAddress {
+  private String company;
+  private String title;
+  private String firstName;
+  private String lastName;
+  private String streetNameAndNumber;
+  private String zipNumber;
+  private String country;
+  private String email;
+  private String phoneNumber;
+  private String addressAddition;
+  private String poBox;
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getStreetNameAndNumber() {
+    return streetNameAndNumber;
+  }
+
+  public void setStreetNameAndNumber(String streetNameAndNumber) {
+    this.streetNameAndNumber = streetNameAndNumber;
+  }
+
+  public String getZipNumber() {
+    return zipNumber;
+  }
+
+  public void setZipNumber(String zipNumber) {
+    this.zipNumber = zipNumber;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getAddressAddition() {
+    return addressAddition;
+  }
+
+  public void setAddressAddition(String addressAddition) {
+    this.addressAddition = addressAddition;
+  }
+
+  public String getPObox() {
+    return poBox;
+  }
+
+  public void setPObox(String poBox) {
+    this.poBox = poBox;
+  }
+
+}

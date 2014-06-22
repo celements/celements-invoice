04d54c5c11162decd96e903948b08fe277fb8e95
Fabian Pichler <fabian.pichler@synventis.com>
2014-06-22 12:37:49 +0200

    implement storeInvoice in XObjectInvoiceSubscrStore

--- component-subscription/src/main/java/com/celements/invoice/subscription/store/XObjectInvoiceSubscrStore.java
+++ component-subscription/src/main/java/com/celements/invoice/subscription/store/XObjectInvoiceSubscrStore.java
@@ -1,13 +1,43 @@
 package com.celements.invoice.subscription.store;
 
-import org.xwiki.component.annotation.Component;
+import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.context.Execution;
+
+import com.celements.common.classes.IClassCollectionRole;
+import com.celements.invoice.InvoiceClassCollection;
 import com.celements.invoice.builder.IInvoice;
+import com.celements.invoice.builder.IInvoiceReferenceDocument;
 import com.celements.invoice.store.IInvoiceStoreExtenderRole;
+import com.celements.invoice.subscription.InvoiceSubscrReferenceDocument;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 
 @Component("celInvoiceSubscr.xobject")
 public class XObjectInvoiceSubscrStore implements IInvoiceStoreExtenderRole {
+
+  private static Log LOGGER = LogFactory.getFactory().getInstance(
+      XObjectInvoiceSubscrStore.class);
+
+  @Requirement("com.celements.invoice.classcollection")
+  IClassCollectionRole invoiceClasses;
+
+  @Requirement
+  Execution execution;
+
+  private XWikiContext getContext() {
+    return (XWikiContext)execution.getContext().getProperty("xwikicontext");
+  }
+
+  private InvoiceClassCollection getInvoiceClasses() {
+    return (InvoiceClassCollection) invoiceClasses;
+  }
 
   public void loadInvoice(XWikiDocument invoiceDoc, IInvoice invoice) {
     // TODO Auto-generated method stub
@@ -15,8 +45,40 @@ public class XObjectInvoiceSubscrStore implements IInvoiceStoreExtenderRole {
   }
 
   public void storeInvoice(IInvoice theInvoice, XWikiDocument invoiceDoc) {
-    // TODO Auto-generated method stub
+    List<IInvoiceReferenceDocument> invReferenceDocs = theInvoice.getReferenceDocs();
+    for (IInvoiceReferenceDocument invRefDoc : invReferenceDocs) {
+      if (invRefDoc instanceof InvoiceSubscrReferenceDocument) {
+        InvoiceSubscrReferenceDocument invSubRefDoc =
+            (InvoiceSubscrReferenceDocument) invRefDoc;
+        try {
+          BaseObject subscrObj = getOrCreateSubscrObject(invoiceDoc);
+          subscrObj.setStringValue(InvoiceClassCollection.FIELD_INVOICE_SUBSCR_REF,
+              invSubRefDoc.getSubscriptionReference());
+          subscrObj.setDateValue(InvoiceClassCollection.FIELD_INVOICE_SUBSCR_FROM,
+              invSubRefDoc.getFrom());
+          subscrObj.setDateValue(InvoiceClassCollection.FIELD_INVOICE_SUBSCR_TO,
+              invSubRefDoc.getTo());
+        } catch (XWikiException exp) {
+          LOGGER.error("Failed to store invoice subscription details on ["
+              + invoiceDoc.getDocumentReference() + "].", exp);
+        }
+      }
+    }
+  }
 
+  BaseObject getOrCreateSubscrObject(XWikiDocument invoiceDoc)
+      throws XWikiException {
+    BaseObject invoiceObj = invoiceDoc.getXObject(getInvoiceClasses(
+        ).getSubscriptionItemClassRef(getWikiName()));
+    if (invoiceObj == null) {
+      invoiceObj = invoiceDoc.newXObject(getInvoiceClasses().getSubscriptionItemClassRef(
+          getWikiName()), getContext());
+    }
+    return invoiceObj;
+  }
+
+  private String getWikiName() {
+    return getContext().getDatabase();
   }
 
 }
--- component-subscription/src/test/java/com/celements/invoice/subscription/store/XObjectInvoiceSubscrStore_storeInvoiceTest.java
+++ component-subscription/src/test/java/com/celements/invoice/subscription/store/XObjectInvoiceSubscrStore_storeInvoiceTest.java
@@ -1,37 +1,151 @@
 package com.celements.invoice.subscription.store;
 
+import static junit.framework.Assert.*;
+import static org.easymock.EasyMock.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.xwiki.model.reference.DocumentReference;
 
+import com.celements.common.classes.IClassCollectionRole;
 import com.celements.common.test.AbstractBridgedComponentTestCase;
+import com.celements.invoice.InvoiceClassCollection;
 import com.celements.invoice.builder.IInvoice;
+import com.celements.invoice.builder.IInvoiceReferenceDocument;
 import com.celements.invoice.store.IInvoiceStoreExtenderRole;
+import com.celements.invoice.subscription.InvoiceSubscrReferenceDocument;
+import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+import com.xpn.xwiki.objects.classes.BaseClass;
 import com.xpn.xwiki.web.Utils;
 
 public class XObjectInvoiceSubscrStore_storeInvoiceTest extends AbstractBridgedComponentTestCase {
 
   private XObjectInvoiceSubscrStore invoiceSubscrStore;
   private XWikiContext context;
+  private XWiki xwiki;
 
   @Before
   public void setUp_InvoiceSubscrStore_loadInvoiceTest() throws Exception {
     context = getContext();
+    xwiki = getWikiMock();
     invoiceSubscrStore = (XObjectInvoiceSubscrStore) Utils.getComponent(
         IInvoiceStoreExtenderRole.class, "celInvoiceSubscr.xobject");
   }
 
   @Test
-  public void testStoreInvoice_nothingToDo() {
+  public void testStoreInvoice_emptyRefDocList() {
     DocumentReference invoiceDocRef = new DocumentReference(context.getDatabase(),
         "Invoices", "Invoice1");
     IInvoice invoice = Utils.getComponent(IInvoice.class);
     XWikiDocument invoiceDoc = new XWikiDocument(invoiceDocRef);
     replayDefault();
     invoiceSubscrStore.storeInvoice(invoice, invoiceDoc);
+    BaseObject subscrObj = invoiceDoc.getXObject(getInvoiceClasses(
+        ).getSubscriptionItemClassRef(context.getDatabase()));
+    assertNull("wrong subscription object on invoice found.", subscrObj);
     verifyDefault();
+  }
+
+  @Test
+  public void testStoreInvoice_noSubRefDoc_in_refDocList() {
+    DocumentReference invoiceDocRef = new DocumentReference(context.getDatabase(),
+        "Invoices", "Invoice1");
+    IInvoice invoice = Utils.getComponent(IInvoice.class);
+    IInvoiceReferenceDocument invoiceRefDoc = createMockAndAddToDefault(
+        IInvoiceReferenceDocument.class);
+    invoice.addInvoiceReferenceDocument(invoiceRefDoc);
+    XWikiDocument invoiceDoc = new XWikiDocument(invoiceDocRef);
+    replayDefault();
+    invoiceSubscrStore.storeInvoice(invoice, invoiceDoc);
+    BaseObject subscrObj = invoiceDoc.getXObject(getInvoiceClasses(
+        ).getSubscriptionItemClassRef(context.getDatabase()));
+    assertNull("wrong subscription object on invoice found.", subscrObj);
+    verifyDefault();
+  }
+
+  @Test
+  public void testStoreInvoice_SubscriptionReference() throws Exception {
+    DocumentReference invoiceDocRef = new DocumentReference(context.getDatabase(),
+        "Invoices", "Invoice1");
+    Calendar cal = Calendar.getInstance();
+    Date to = cal.getTime();
+    cal.add(Calendar.MONTH, -1);
+    Date from = cal.getTime();
+    IInvoice invoice = Utils.getComponent(IInvoice.class);
+    InvoiceSubscrReferenceDocument invoiceRefDoc = (InvoiceSubscrReferenceDocument)
+        Utils.getComponent(IInvoiceReferenceDocument.class, "subscription");
+    invoiceRefDoc.setFrom(from);
+    invoiceRefDoc.setTo(to);
+    String subscrRef = "Abonnement.Reference";
+    invoiceRefDoc.setSubscriptionReference(subscrRef);
+    invoice.addInvoiceReferenceDocument(invoiceRefDoc);
+    XWikiDocument invoiceDoc = new XWikiDocument(invoiceDocRef);
+    assertFalse("Precondition failure 'to maynot equal from'", to.equals(from));
+    DocumentReference subscrItemClassRef = getInvoiceClasses(
+        ).getSubscriptionItemClassRef(context.getDatabase());
+    BaseClass invClassMock = createMockAndAddToDefault(BaseClass.class);
+    expect(xwiki.getXClass(eq(subscrItemClassRef), same(context))).andReturn(invClassMock);
+    expect(invClassMock.newCustomClassInstance(same(context))).andReturn(
+        new BaseObject()).once();
+    replayDefault();
+    invoiceSubscrStore.storeInvoice(invoice, invoiceDoc);
+    BaseObject subscrObj = invoiceDoc.getXObject(subscrItemClassRef);
+    assertNotNull("subscription object on invoice document missing.", subscrObj);
+    assertEquals(subscrRef, subscrObj.getStringValue(
+        InvoiceClassCollection.FIELD_INVOICE_SUBSCR_REF));
+    assertEquals(from, subscrObj.getDateValue(
+        InvoiceClassCollection.FIELD_INVOICE_SUBSCR_FROM));
+    assertEquals(to, subscrObj.getDateValue(
+        InvoiceClassCollection.FIELD_INVOICE_SUBSCR_TO));
+    verifyDefault();
+  }
+  
+  @Test
+  public void testStoreInvoice_update_SubscriptionReference() throws Exception {
+    DocumentReference invoiceDocRef = new DocumentReference(context.getDatabase(),
+        "Invoices", "Invoice1");
+    Calendar cal = Calendar.getInstance();
+    Date to = cal.getTime();
+    cal.add(Calendar.MONTH, -1);
+    Date from = cal.getTime();
+    IInvoice invoice = Utils.getComponent(IInvoice.class);
+    InvoiceSubscrReferenceDocument invoiceRefDoc = (InvoiceSubscrReferenceDocument)
+        Utils.getComponent(IInvoiceReferenceDocument.class, "subscription");
+    invoiceRefDoc.setFrom(from);
+    invoiceRefDoc.setTo(to);
+    String subscrRef = "Abonnement.Reference";
+    invoiceRefDoc.setSubscriptionReference(subscrRef);
+    invoice.addInvoiceReferenceDocument(invoiceRefDoc);
+    XWikiDocument invoiceDoc = new XWikiDocument(invoiceDocRef);
+    assertFalse("Precondition failure 'to maynot equal from'", to.equals(from));
+    DocumentReference subscrItemClassRef = getInvoiceClasses(
+        ).getSubscriptionItemClassRef(context.getDatabase());
+    BaseObject currentSubObject = new BaseObject();
+    currentSubObject.setXClassReference(subscrItemClassRef);
+    invoiceDoc.addXObject(currentSubObject);
+    replayDefault();
+    invoiceSubscrStore.storeInvoice(invoice, invoiceDoc);
+    assertEquals(1, invoiceDoc.getXObjects(subscrItemClassRef).size());
+    BaseObject subscrObj = invoiceDoc.getXObject(subscrItemClassRef);
+    assertNotNull("subscription object on invoice document missing.", subscrObj);
+    assertEquals(subscrRef, subscrObj.getStringValue(
+        InvoiceClassCollection.FIELD_INVOICE_SUBSCR_REF));
+    assertEquals(from, subscrObj.getDateValue(
+        InvoiceClassCollection.FIELD_INVOICE_SUBSCR_FROM));
+    assertEquals(to, subscrObj.getDateValue(
+        InvoiceClassCollection.FIELD_INVOICE_SUBSCR_TO));
+    verifyDefault();
+  }
+
+  private InvoiceClassCollection getInvoiceClasses() {
+    return (InvoiceClassCollection) Utils.getComponent(IClassCollectionRole.class,
+        "com.celements.invoice.classcollection");
   }
 
 }

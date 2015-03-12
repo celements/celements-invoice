e259c6c27b07595c8f25801e05a93df4003851e3
Marc Sladek <marc.sladek@synventis.com>
2015-03-12 23:32:45 +0100

    InvoiceScriptService.getInvoice() and getInvoiceItem() added

--- component/src/main/java/com/celements/invoice/service/InvoiceScriptService.java
+++ component/src/main/java/com/celements/invoice/service/InvoiceScriptService.java
@@ -6,6 +6,9 @@ import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.script.service.ScriptService;
 
 import com.celements.invoice.InvoiceClassCollection;
+import com.celements.invoice.builder.IInvoice;
+import com.celements.invoice.builder.IInvoiceItem;
+import com.celements.invoice.store.IInvoiceStoreRole;
 
 @Component("celInvoice")
 public class InvoiceScriptService implements ScriptService {
@@ -13,8 +16,19 @@ public class InvoiceScriptService implements ScriptService {
   @Requirement("xobject")
   IInvoiceServiceRole invoiceService;
 
+  @Requirement("xobject")
+  IInvoiceStoreRole invoiceStore;
+
   public DocumentReference getInvoiceDocRefForInvoiceNumber(String invoiceNumber) {
     return invoiceService.getInvoiceDocRefForInvoiceNumber(invoiceNumber);
+  }
+
+  public IInvoice getInvoice(DocumentReference invoiceDocRef) {
+    return invoiceStore.loadInvoice(invoiceDocRef);
+  }
+
+  public IInvoiceItem getInvoiceItem(DocumentReference invoiceDocRef, int position) {
+    return invoiceStore.loadInvoiceItem(invoiceDocRef, position);
   }
 
   public String getInvoiceClass() {
